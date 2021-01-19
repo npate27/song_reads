55047ea7358a8caefb9e87a15f767f5aee16b86c
Neel Patel <neelpatel0596@gmail.com>
2021-01-18 21:49:19 -0500

    Populate Song Source list using BLoC

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,63 @@
 import 'package:flutter/material.dart' hide Router;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:song_reads/bloc/blocs.dart';
+import 'package:song_reads/clients/clients.dart';
+import 'package:song_reads/clients/http_client_singleton.dart';
+import 'package:song_reads/repositories/reddit_repository.dart';
+import 'package:song_reads/repositories/repositories.dart';
 import 'package:song_reads/router.dart';
 import 'package:song_reads/constants/routes.dart' as RouterConstants;
 import 'package:song_reads/constants/literals.dart' as LiteralConstants;
 import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(SongReads());
-  debugPaintSizeEnabled = false;
+/// Custom [BlocObserver] which observes all bloc and cubit instances.
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    print(event);
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onChange(Cubit cubit, Change change) {
+    print(change);
+    super.onChange(cubit, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(cubit, error, stackTrace);
+  }
 }
+
 
 class SongReads extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: LiteralConstants.appName,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocProvider(
+      create: (BuildContext context) => SearchSourceBloc(ytRepository: YouTubeRepository(apiClient: YouTubeApiClient(httpClient: AppHttpClient().client)), redditRepository: RedditRepository(apiClient: RedditApiClient(httpClient: AppHttpClient().client))),
+      child: MaterialApp(
+          title: LiteralConstants.appName,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          onGenerateRoute: Router.generateRoute,
+          initialRoute: RouterConstants.mainRoute
       ),
-      onGenerateRoute: Router.generateRoute,
-      initialRoute: RouterConstants.mainRoute
     );
   }
 }
-
+void main() {
+  Bloc.observer = SimpleBlocObserver();
+  runApp(SongReads());
+  debugPaintSizeEnabled = false;
+}
