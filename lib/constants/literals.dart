47e59686e0dcd1f31dfafc338a2200d41f63c3bf
Neel Patel <neelpatel0596@gmail.com>
2021-01-25 05:24:54 -0500

    Remove extra whitespace

--- lib/constants/literals.dart
+++ lib/constants/literals.dart
@@ -1,13 +1,13 @@
 library literals;
 
-// ===== General  =====
+// ===== General =====
 const String appName = 'Song Reads';
 const String songCommentHeader = 'Song Comments';
 const String albumCommentHeader = 'Album Comments';
 const String atGeniusString = '@ Genius';
 const String nowPlayingCardHeader = 'Now Playing';
 
-// ===== API  =====
+// ===== API =====
 const String baseGeniusApiUrl = 'https://api.genius.com';
 const String baseYoutubeApiUrl = 'https://youtube.googleapis.com/youtube/v3';
 const String baseRedditApiUrl = 'https://reddit.com';
