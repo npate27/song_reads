2abb8c75f888e37314749da372baa4ce40071ac9
Neel Patel <neelpatel0596@gmail.com>
2021-05-09 06:20:26 -0400

    Add OpenContainer to results with Hero animation to details page.

--- lib/components/comment_source_result_card.dart
+++ lib/components/comment_source_result_card.dart
@@ -1,8 +1,10 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:song_reads/components/comment_source_info.dart';
 import 'package:song_reads/constants/enums.dart';
 import 'package:song_reads/constants/routes.dart' as RouterConstants;
 import 'package:song_reads/models/models.dart';
+import 'package:song_reads/pages/comments_page.dart';
 
 class CommentSourceResultCardItem extends StatelessWidget {
   final Source sourceData;
@@ -12,59 +14,69 @@ class CommentSourceResultCardItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     CommentSource commentSource = sourceData.commentSource;
-    return InkWell(
-      onTap: () { Navigator.pushNamed(context, RouterConstants.commentsRoute, arguments: sourceData); },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-        height: 100,
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50),
-        ),
+    return OpenContainer(
+      tappable: true,
+      closedElevation: 0,
+      closedShape: const RoundedRectangleBorder(),
+      openBuilder: (BuildContext context, void Function({Object returnValue}) action) {
+        return CommentsPage(sourceData: sourceData);
+      },
+      closedBuilder: (BuildContext context, void Function() action) {
+        return Hero(
+          tag: '${sourceData.commentSource.inString}-${sourceData.id}',
           child: Container(
-            decoration: BoxDecoration(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            height: 100,
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(50),
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  stops: [0.25, 1],
-                  colors: [commentSource.sourceImageBaseColor, commentSource.sourceImageBaseColor.withOpacity(0.35)],
-                )
-            ),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    flex: 8,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      stops: [0.25, 1],
+                      colors: [commentSource.sourceImageBaseColor, commentSource.sourceImageBaseColor.withOpacity(0.35)],
+                    )
+                ),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        flex: 8,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                              child: Image.asset(commentSource.sourceImagePath),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                                  child: Image.asset(commentSource.sourceImagePath),
+                                ),
+                                //TODO: There's gotta be a better way of doing this surely
+                                Flexible(child: Container(child: CommentSourceInfo(sourceData: sourceData,)))
+                              ],
                             ),
-                            //TODO: There's gotta be a better way of doing this surely
-                            Flexible(child: Container(child: CommentSourceInfo(sourceData: sourceData,)))
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                      child: Icon(Icons.arrow_forward_ios_sharp, color: Colors.white,),
-                    ),
-                  )
-                ]
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: Icon(Icons.arrow_forward_ios_sharp, color: Colors.white,),
+                        ),
+                      )
+                    ]
+                ),
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/pages/comments_page.dart
+++ lib/pages/comments_page.dart
@@ -4,6 +4,7 @@ import 'package:song_reads/bloc/blocs.dart';
 import 'package:song_reads/clients/clients.dart';
 import 'package:song_reads/clients/http_client_singleton.dart';
 import 'package:song_reads/components/comment_info_view.dart';
+import 'package:song_reads/components/comment_source_info.dart';
 import 'package:song_reads/components/custom_loading_indicator.dart';
 import 'package:song_reads/constants/enums.dart';
 import 'package:song_reads/models/comment_info.dart';
@@ -27,14 +28,67 @@ class _CommentsPageState extends State<CommentsPage> {
 
   @override
   Widget build(BuildContext context) {
+    Source sourceData = widget.sourceData;
+    CommentSource commentSource = sourceData.commentSource;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(child: Expanded(child: songSearchCommentsBlocBuilder(bloc, widget.sourceData.id, widget.sourceData.commentSource)))
-          ],
-        ),
+      body: Column(
+        children: [
+          Hero(
+          tag: '${sourceData.commentSource.inString}-${sourceData.id}',
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+              height: 100,
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        stops: [0.25, 1],
+                        colors: [commentSource.sourceImageBaseColor, commentSource.sourceImageBaseColor.withOpacity(0.35)],
+                      )
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          flex: 8,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                                    child: Image.asset(commentSource.sourceImagePath),
+                                  ),
+                                  //TODO: There's gotta be a better way of doing this surely
+                                  Flexible(child: Container(child: CommentSourceInfo(sourceData: sourceData,)))
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Flexible(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                            child: Icon(Icons.arrow_forward_ios_sharp, color: Colors.white,),
+                          ),
+                        )
+                      ]
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(child: Expanded(child: songSearchCommentsBlocBuilder(bloc, widget.sourceData.id, widget.sourceData.commentSource)))
+        ],
       ),
     );
   }
